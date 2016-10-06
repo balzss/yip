d12302bf0dad504509bd037a87437ff0d30920c5
balazs.saros <balazs.saros@gmail.com>
2016-10-06 18:34:24 +0200

    init commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='yip',
+    version='0.2',
+    description='User-friendly frontend for \
+            searching pip, like yaourt for pacman',
+    url='https://github.com/balzss/yip',
+    author='balzss',
+    author_email='balazs.saros@gmail.com',
+    license='GPLv3',
+    keywords='pip frontend user-friendly CLI command-line',
+    scripts=['yip']
+)
