4ced550391b3d3a799bd199567aa5a1bc3ccca70
balazs.saros <balazs.saros@gmail.com>
2016-10-18 18:49:11 +0200

    1.0 release

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='yip',
-    version='0.4',
-    description='User-friendly frontend for \
-            searching pip, like yaourt for pacman',
+    version='1.0',
+    description='Frontend for searching PyPI, a feature rich alternative \
+            to pip search',
     url='https://github.com/balzss/yip',
     author='balzss',
     author_email='balazs.saros@gmail.com',
